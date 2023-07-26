8ae19ce38fedbf9341b91f98fa0fcf81ebc10bfd
Diana Pugaci <pugaci.diana@gmail.com>
2023-07-26 08:10:21 +0300

    commit

--- settings.py
+++ settings.py
@@ -5,21 +5,12 @@ from exceptions.exceptions import ConfigNotFoundException, ConfigJSONException, 
 
 
 class Settings:
+
     _instance = None
 
     CONFIG_PATH = "./config.json"
 
     WINDOW_SIZE = (("window", "width"), ("window", "height"))
-
-    SPRITES = "sprite"
-
-    DIRECTION_TOP = ("direction", "top")
-
-    DIRECTION_LEFT = ("direction", "left")
-
-    DIRECTION_DOWN = ("direction", "down")
-
-    DIRECTION_RIGHT = ("direction", "right")
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -29,7 +20,7 @@ class Settings:
     def __init__(self):
         self._open_config(self.CONFIG_PATH)
 
-    def _open_config(self, path):
+    def _open_config(self, path: str) -> None:
         try:
             with open(path) as f:
                 self._config = json.load(f)
@@ -38,12 +29,12 @@ class Settings:
         except JSONDecodeError as e:
             raise ConfigJSONException(e.msg)
 
-    def get(self, args):
+    def get(self, args) -> tuple:
         if type(args[0]) == tuple or type(args[0]) == list:
             return self.get_tuple(args)
         return self.get_constant(args)
 
-    def get_constant(self, args: tuple | list):
+    def get_constant(self, args: tuple | list) -> tuple:
         config = self._config
         try:
             for constant in args:
@@ -52,7 +43,7 @@ class Settings:
         except KeyError as e:
             raise ConfigKeyException(e.args[0])
 
-    def get_tuple(self, args_list: tuple[tuple] | list[tuple]):
+    def get_tuple(self, args_list: tuple[tuple] | list[tuple] | list[list]) -> tuple:
         constants = []
         for args in args_list:
             constants.append(self.get(args))
